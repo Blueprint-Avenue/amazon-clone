3be5de6bfc622af7381b9f7e7047f34e6f58b90d
Samiya <skmcintosh20@gmail.com>
2022-04-18 17:46:56 -0700

    Completed Header Component and styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-	return <div className="App">JELLO</div>;
+	return (
+		<div className="App">
+			{/* Header */}
+			<Header />
+			{/* Home */}
+		</div>
+	);
 }
 
 export default App;
